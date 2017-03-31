bf261801cd17fd66c12d25cb77626441188d34e3
Aditya Gupta <aditya@taazaa.com>
2017-04-01 00:08:33 +0530

    Plugin to use webpack as grunt task in grunt

--- ebookstore/config/dev/webpack.config.js
+++ ebookstore/config/dev/webpack.config.js
@@ -2,6 +2,13 @@
     ./webpack.config.js
 */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: '../../assets/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: '../../client/index.js',
   output: {
@@ -14,4 +21,5 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   }
+  plugins: [HtmlWebpackPluginConfig]
 }
