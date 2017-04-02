22e3e61cff9cd21d8daf5fb50d98d33683009e44
Aditya Gupta <aditya@taazaa.com>
2017-04-02 22:12:19 +0530

    Sign up form width fixed

--- ebookstore/client/components/header/index.js
+++ ebookstore/client/components/header/index.js
@@ -67,7 +67,7 @@ export class Header extends Component {
       <div className="row" style={{background: '#000'}}>
         <Nav className="col-sm-offset-6 col-xs-offset-3 col-sm-3 col-xs-3"></Nav>
         <div className="col-sm-3 col-xs-6">
-          <Modal show={this.state.showSignUpModal} title='Sign Up'
+          <Modal modalWidth="35%" show={this.state.showSignUpModal} title='Sign Up'
             body={SignUpForm} actionText='Complete' closeHandler={this.signUpClose} actionHandler={this.signUpSave}  />
           <button type="button" className="btn btn-primary" onClick={this.signUpOpen}>Sign Up</button>
           &nbsp;&nbsp;
