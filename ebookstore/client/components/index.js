2741a757f20b09baf51d0f0e98f969018d56328a
unknown <vasukumar169@gmail.com>
2017-04-02 22:00:07 +0530

    Added Sign Up Form

--- ebookstore/client/components/index.js
+++ ebookstore/client/components/index.js
@@ -4,3 +4,30 @@ import { NotFound } from './default/';
 import { Home } from './home/';
 
 export { Address, Container, NotFound, Home };
+export class SignUpForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render(){
+    return (
+      <form name="SignUpForm" id="SignUpForm" method="post">
+        <div className="form-group">
+            <input className="form-control" id="userinfo" placeholder="First Name" name="userinfo" type="text" />
+        </div>
+        <div className="form-group">
+            <input className="form-control" id="userinfo" placeholder="Last Name" name="userinfo" type="text" />
+        </div>
+        <div className="form-group">
+            <input className="form-control" id="userinfo" placeholder="User Name" name="userinfo" type="text" />
+        </div>
+        <div className="form-group">
+            <input className="form-control" id="password" placeholder="Password" name="password" type="password" value="" />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default SignInForm;
