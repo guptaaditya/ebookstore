ab9e7236f788c72106919edb2a74eab2ad6e7196
Aditya Gupta <aditya@taazaa.com>
2017-04-03 00:53:42 +0530

    Introduction of actions and reducers

---
+++ ebookstore/client/actions/auth.js
@@ -0,0 +1,3 @@
+export default function signUp(user){
+  return {type: 'user/create', user}
+}
--- ebookstore/client/components/signUp/index.js
+++ ebookstore/client/components/signUp/index.js
@@ -4,37 +4,70 @@ import Modal from '../modal/'
 class SignUpInput extends Component {
   constructor(props) {
     super(props)
+    this.userNameChange = this.userNameChange.bind(this)
+    this.passwordChange = this.passwordChange.bind(this)
+    this.firstNameChange = this.firstNameChange.bind(this)
+    this.lastNameChange = this.lastNameChange.bind(this)
+  }
+
+  firstNameChange(event){
+    this.props.optionalMethod({firstName: event.target.value})
+  }
+
+  lastNameChange(event) {
+    this.props.optionalMethod({lastName: event.target.value})
+  }
+
+  passwordChange(event){
+    this.props.optionalMethod({password: event.target.value})
+  }
+
+  userNameChange(event) {
+    this.props.optionalMethod({userName: event.target.value})
   }
 
   render(){
     return (
       <form name="SignUp" id="SignUp" method="post">
         <div className="form-group">
-            <input className="form-control" id="userinfo" placeholder="First Name" name="userinfo" type="text" />
+            <input className="form-control" placeholder="First name" type="text" onChange={this.firstNameChange} />
         </div>
         <div className="form-group">
-            <input className="form-control" id="userinfo" placeholder="Last Name" name="userinfo" type="text" />
+            <input className="form-control" placeholder="Last name" type="text" onChange={this.lastNameChange} />
         </div>
         <div className="form-group">
-            <input className="form-control" id="userinfo" placeholder="User Name" name="userinfo" type="text" />
+            <input className="form-control" placeholder="User name" type="text" onChange={this.userNameChange} />
         </div>
         <div className="form-group">
-            <input className="form-control" id="password" placeholder="Password" name="password" type="password" value="" />
+            <input className="form-control" placeholder="Password" type="password" onChange={this.passwordChange} />
         </div>
       </form>
     )
   }
 }
 
+SignUpInput.propTypes = {
+  updateChanges: React.PropTypes.func.isRequired,
+}
+
+SignUpInput.defaultProps = {
+  updateChanges: (event) => console.log('From input component',event),
+}
+
 export class SignUp extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showSignUpModal: false
+      showSignUpModal: false,
+      firstName: '',
+      lastName: '',
+      userName: '',
+      passWord: ''
     }
     this.signUpClose = this.signUpClose.bind(this)
     this.signUpOpen = this.signUpOpen.bind(this)
     this.signUpSave = this.signUpSave.bind(this)
+    this.updateChanges = this.updateChanges.bind(this);
   }
 
   signUpClose() {
@@ -50,13 +83,17 @@ export class SignUp extends Component {
   }
 
   signUpSave() {
+    alert(this.state.userName+this.state.password);
+  }
 
+  updateChanges(changedData) {
+    this.setState(Object.assign(this.state, changedData))
   }
 
   render(){
     return (
       <span>
-        <Modal modalWidth="35%" show={this.state.showSignUpModal} title='Sign Up'
+        <Modal modalWidth="35%" show={this.state.showSignUpModal} title='Sign Up' optionalBodyProp={this.updateChanges}
           body={SignUpInput} actionText='Complete' closeHandler={this.signUpClose} actionHandler={this.signUpSave}  />
         <button type="button" className="btn btn-primary" onClick={this.signUpOpen}>Sign Up</button>
       </span>
